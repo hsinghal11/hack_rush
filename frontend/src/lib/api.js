316821275b1a8e60331481f628637048b8bf3b06
Your Name <youremail@yourdomain.com>
2025-04-06 09:34:44 +0530

    api_changes

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://hack-rush.onrender.com/api';
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://hack-rush.onrender.com/api';
+// const API_URL = 'http://localhost:8000/api';
 
 
 // Create an axios instance with baseURL
